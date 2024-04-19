4226f508374d64296656311f21e16af6a1207b2c
Daniel <danieldaniel22012201@gmail.com>
2024-04-19 20:58:18 +0800

    update

--- exercises/traits/traits5.rs
+++ exercises/traits/traits5.rs
@@ -7,15 +7,16 @@
 // Execute `rustlings hint traits5` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 pub trait SomeTrait {
+    // default method for a type that implements SomeTrait
     fn some_function(&self) -> bool {
         true
     }
 }
 
 pub trait OtherTrait {
+    // default method for a type that implements OtherTrait
     fn other_function(&self) -> bool {
         true
     }
@@ -30,7 +31,7 @@ impl SomeTrait for OtherStruct {}
 impl OtherTrait for OtherStruct {}
 
 // YOU MAY ONLY CHANGE THE NEXT LINE
-fn some_func(item: ??) -> bool {
+fn some_func(item: impl SomeTrait + OtherTrait) -> bool {
     item.some_function() && item.other_function()
 }
 
